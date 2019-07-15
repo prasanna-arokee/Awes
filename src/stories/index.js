8641f30088809c6abb22ee07c9e2c11705f0f93c
root <prasanna_williams>
2019-07-15 19:04:51 +0530

    First commiut'

---
+++ src/stories/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import Button2 from "./Button/AddButton";
+import './Button/AddButton.css'
+import { Button, Welcome } from '@storybook/react/demo';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button2', module).add('to Storybook', () => <Button2 showApp={linkTo('Button2')} />);
+
+
+// storiesOf('Button', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emoji', () => (
+//     <Button onClick={action('clicked')}>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍 💯
+//       </span>
+//     </Button>
+//   ));
